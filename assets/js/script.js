defc2f23114cca5f754e53554d757f8943e2f37a
Luis <luisvalladares79@gmail.com>
2024-05-01 20:07:14 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function toggleElement() {
+    let mostrarSociales = document.getElementById("cuadro-social");
+    if (mostrarSociales.style.display === "none") {
+      mostrarSociales.style.display = "flex";
+    } else {
+      mostrarSociales.style.display = "none";
+    }
+  }
+  
